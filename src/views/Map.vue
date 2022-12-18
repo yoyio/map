a1eedbd518fc61b85711420524d7b5f50fcebf67
Zhi901210 <qqqqq901210@gmail.com>
2022-12-19 03:43:10 +0800

    側邊攔照片

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
     <div class="Information">
+      <img :src="img" style="width: 100%; height: 30%" />
       <p class="Information-title">{{ name }}</p>
       <p class="Information-text">{{ area }}</p>
       <p class="Information-text">{{ c }}</p>
@@ -8,12 +9,12 @@
       <P class="Information-text">{{ proele }}</P>
       <P class="Information-text">{{ co2 }}</P>
       <P class="Information-text">{{ reduceCo2 }}</P>
-      <RouterLink
+      <!-- <RouterLink
         to="/Information"
         class=""
         style="font-size: 15px; color: #3f3f3f"
         >更多資訊</RouterLink
-      >
+      > -->
     </div>
     <!--地圖呈現在此-->
     <div class="mapContainer" ref="mapContainer"></div>
@@ -30,6 +31,15 @@ import allData from "../data/xinyi.json";
 import { onMounted, ref } from "vue";
 
 const mapContainer = ref(null);
+const img = ref(null);
+const name = ref(null);
+const area = ref(null);
+const c = ref(null);
+const amount = ref(null);
+const proele = ref(null);
+const co2 = ref(null);
+const reduceCo2 = ref(null);
+
 const markers = L.markerClusterGroup();
 
 onMounted(() => {
@@ -62,33 +72,42 @@ onMounted(() => {
   //Marker for 迴圈 ,圖標上顯示訊息
   for (let i = 0; allData.restaurants.length > i; i++) {
     markers.addLayer(
-      L.marker([
-        allData.restaurants[i].lat,
-        allData.restaurants[i].lng,
-      ]).bindPopup(
-        `<img src="` +
-          allData.restaurants[i].img +
-          `" style="width: 300px; height: 150px;padding-bottom: 10px;"><div class="InfoWindowOpened-text ">
+      L.marker([allData.restaurants[i].lat, allData.restaurants[i].lng])
+        .bindPopup(
+          `<img src="` +
+            allData.restaurants[i].img +
+            `" style="width: 300px; height: 150px;padding-bottom: 10px;"><div class="InfoWindowOpened-text ">
         <h1>` +
-          allData.restaurants[i].name +
-          `</h1><p style="font-size: 18px;">地址:` +
-          allData.restaurants[i].address +
-          `<br>電話` +
-          allData.restaurants[i].iphon +
-          `<br>申請人類別:` +
-          allData.restaurants[i].whoCategory +
-          `<br>申請人或機構:` +
-          allData.restaurants[i].who +
-          `<br>設置型別:` +
-          allData.restaurants[i].category +
-          `<br>發電設備種類:` +
-          allData.restaurants[i].electricity +
-          `(PV)<br>設置位置:` +
-          allData.restaurants[i].location +
-          `<br>總裝置容量:` +
-          allData.restaurants[i].capacity +
-          `kW</p> `
-      )
+            allData.restaurants[i].name +
+            `</h1><p style="font-size: 18px;">地址:` +
+            allData.restaurants[i].address +
+            `<br>電話` +
+            allData.restaurants[i].iphon +
+            `<br>申請人類別:` +
+            allData.restaurants[i].whoCategory +
+            `<br>申請人或機構:` +
+            allData.restaurants[i].who +
+            `<br>設置型別:` +
+            allData.restaurants[i].category +
+            `<br>發電設備種類:` +
+            allData.restaurants[i].electricity +
+            `(PV)<br>設置位置:` +
+            allData.restaurants[i].location +
+            `<br>總裝置容量:` +
+            allData.restaurants[i].capacity +
+            `kW</p> `
+        )
+        .on("click", () => {
+          img.value = allData.restaurants[i].img;
+          name.value = allData.restaurants[i].name;
+          area.value = "占地面積: " + allData.restaurants[i].area + "公頃";
+          c.value = "漁獲: " + allData.restaurants[i].c;
+          amount.value = "產量: " + allData.restaurants[i].amount + "公斤";
+          proele.value = "發電量: " + allData.restaurants[i].proele + "KW";
+          co2.value = "碳排量: " + allData.restaurants[i].co2 + "公斤";
+          reduceCo2.value =
+            "減少碳排量: " + allData.restaurants[i].reduceCo2 + "公斤";
+        })
     );
   }
   map.addLayer(markers);
@@ -97,25 +116,24 @@ onMounted(() => {
 
 <style scoped>
 .container {
-  max-width: 1300px;
+  max-width: 1500px;
   margin: 0px auto;
   padding: 10px;
   display: flex;
   flex-direction: row;
 }
 .Information {
-  background-color: bisque;
   width: 20%;
   height: 700px;
 }
 .Information-title {
   color: #3f3f3f;
-  font-size: 35px;
-  padding: 50px 30px;
+  font-size: 40px;
+  padding: 20px 20px;
 }
 .Information-text {
   color: #3f3f3f;
-  font-size: 15px;
+  font-size: 20px;
   padding: 3px 30px;
 }
 .google-map {
