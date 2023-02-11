7d66672d5b3910b264f5ef087340d0759a29e3d3
QQ9897 <qqqforprogram@gmail.com>
2023-02-12 01:05:48 +0800

    no message

--- src/views/about.vue
+++ src/views/about.vue
@@ -1,20 +1,86 @@
 <template>
   <div class="about">
-    <div class="background">
-      <img class="about-img" />
+    <div class="img"></div>
+    <div class="aboutO">
+      <p class="aboutOTitle" style="font-size: 35px;text-align: center;">亞洲大學-精準健康研究中心</p>
+      <p style=" padding: 15px 0px;">本中心以精準營養健康為主要產業發展核心，透過智慧健康產業技術研發與人才培育及軟體
+        平台開發為導向；應用人工智慧、生物資訊、自然語言處理和霎端運算於健康營養產業；整合與分析精準營養大數據、建立混合式雲端健康平台、協助資訊安全等，引領台灣智薏精準營養健康產業及永續城市1社區之發展。
+      </p>
+      <p style=" padding: 15px 0px;">
+        整合醫療大數據與人工智慧，達成精準醫療之目的。精準醫療是未來新的醫療模式，透過醫療大數據之整合基因檢測、疾病治療、日常活動、癒後追蹤等數據，結合人工智慧能協助臨床醫生準確地選擇出適合病人的標靶藥物及相關治療方案，如此一來，不但能降低罹病風險，更可達到預防疾病發生的機會，更有機會提高病人存活率。
+        本研究中心研究主題包括：應用人工智慧與大數據於• 大數據於基因體分析、表觀基因體分析、微生物體體群分析及抗癌藥物的開發，以及開發精準醫療智慧報告系統。
+      </p>
     </div>
-    <div class="about-t">
-      <div class="about-title">
-        <font face="DFKai-sb"><span>亞洲大學</span></font>
-        <font face="DFKai-sb"><span>精準健康研究中心</span></font>
+    <div class="aboutA">
+      <div class="aboutAA">
+        <p class="aboutATitle">三大領域</p>
+        <p style="font-size: 15px;text-align: center;color: #9c9a9a;">透過 精準醫療、智慧醫療、精準營養三大領域，實現對未來AI領域的人才培養與教育水平的提升。</p>
+        <div class="aboutA-item">
+          <hr />
+          <p class="aboutTT">精準醫療</p>
+          <p>Precision Medicine，又稱個性化醫療，簡稱PM，為全球醫療發展趨勢，隨著大數據及AI的快速發展，各國紛紛聚焦於精準醫療發展。</p>
+        </div>
+        <div class="aboutA-item">
+          <hr />
+          <p class="aboutTT">智慧醫療</p>
+          <p>
+            資通訊科技(ICT) 在醫療及健康領域的應用，包括醫療照護、疾病管理、公共衛生監測、教育和研究。
+          </p>
+        </div>
+        <div class="aboutA-item">
+          <hr />
+          <p class="aboutTT">精準營養</p>
+          <p>
+            個性化的營養建議，通過合理飲食和科學的補充營養補劑，實現對疾病的預防、達到健康的目的。
+          </p>
+        </div>
       </div>
-      <div class="about-text">
-        <font face="DFKai-sb"
-          ><span>校址: 413305 台中市霧峰區柳豐路500號</span></font
-        >
-        <font face="DFKai-sb"
-          ><span>TEL: +886-4-2332-3456 | +886-4-3706-1889</span></font
-        >
+      <div class="aboutAA">
+        <p class="aboutATitle">本中心四大研究項目</p>
+        <div class="aboutA-item">
+          <hr />
+          <p class="aboutTT">透過大數據加值整合</p>
+          <p>建立國內第一個精準營養體學及健康大數據。以精準營養為主軸，透過基因體、微生物體、飲食營養、臨床數據以及社經環境因素之分析探勘，使能區分預測個人化之易感性體質，
+            進一步運用於健康促進與疾病防治，以自動化系統評估個人飲食營養，開立飲食處方，並開發各種健康食材與食物。</p>
+        </div>
+        <div class="aboutA-item">
+          <hr />
+          <p class="aboutTT">發展永續循環生態系統流程</p>
+          <p>
+            改善糧食提供模式。目前全球健康永續飲食的主要精神是以植物及綠色水產食物為主，限制紅肉之攝取。過去西方學界之紅肉研究以牛肉為主，因探討不足粗略地將豬肉之風險比照牛肉。然而，豬肉在亞洲人飲食營養中扮演重要角色，且豬肉致慢性病風險及排碳效應均低於牛肉。因此，深入研究以確立豬肉之風險定位。
+          </p>
+        </div>
+        <div class="aboutA-item">
+          <hr />
+          <p class="aboutTT">以分析健康數據為主軸</p>
+          <p>
+            建構基於開源軟體的雲原生運算平台與服務架構，以利進行高靈活、高彈性及高可靠的大規模健康資料分析與預測。應用雲端技術建立精準營養健康資料湖、資料倉儲以及數據交換機制。透過亞洲大學智慧物聯網平台（AUtalk）整合開發工具，提供敏捷的儲存與運算資源，打造高再生的服務架構，幫助開發人員縮短雲端服務開發生命週期，並滿足用戶的需求，進行產業服務。
+          </p>
+        </div>
+        <div class="aboutA-item">
+          <hr />
+          <p class="aboutTT">促進智慧健康產業之升級。</p>
+          <p>
+            以人工智慧與生物資訊技術，提出多體學及循環經濟分析模型、開發建立飲食營養相關之疾病風險評估工具及營養諮詢與健康議題之對話系統，以促進智慧健康產業之升級。
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="aboutB">
+      <p class="aboutATitle">專業核心</p>
+      <div class="aboutBB">
+        <div class="aboutBB-item">
+          <p class="aboutBB-img1"></p>
+          <p class="aboutBB-text">提高研發能力</p>
+        </div>
+        <div class="aboutBB-item">
+          <p class="aboutBB-img2"></p>
+          <p>建立永續發展體系</p>
+        </div>
+        <div class="aboutBB-item">
+          <p class="aboutBB-img3"></p>
+          <p>保持創新思想</p>
+        </div>
       </div>
     </div>
   </div>
@@ -22,48 +88,118 @@
 
 <style scoped>
 .about {
-  width: 100%;
-  height: 100%;
-  margin: 0 auto;
-  background-image: url(".././assets/1.png");
-  background-position: center;
-  background-size: cover;
-}
-.about-img {
-  max-width: 70%;
-  height: 93vh;
-}
-.about-t {
-  width: 60%;
-  height: 500px;
+  max-width: 80%;
   margin: 10px auto;
-  padding: 10px;
-  position: absolute;
-  top: 100px;
-  left: 20%;
-  background-color: #f7f5ed;
-  display: flex;
-  flex-direction: row;
-  justify-content:center;
 }
-.about-title {
-  margin: 10px;
-  padding: 30px;
-  display: flex;
-  flex-direction:column;
-  justify-content:center;
-  align-items:center;
-  font-size: 30px;
-  border-style: solid;
-  border-color: #878787;
-  border-width: 0px 3px 0px 0px;
+
+.img {
+  background-color: rgb(116, 114, 114);
+  width: 100%;
+  height: 60vh;
+  margin: 20px 0px;
 }
-.about-text {
-  margin: 10px;
+
+/*O*/
+.aboutO {
+  width: 100%;
+  margin: 20px auto;
   padding: 20px;
-  display: flex;
-  flex-direction:column;
-  justify-content:center;
+}
+
+/*A*/
+.aboutA {
+  max-width: 100%;
+  margin: 50px auto;
+  padding: 60px 0px 0px 0px;
+}
+
+.aboutATitle {
+  padding-bottom: 20px;
+  font-size: 35px;
+  text-align: center;
+}
+
+.aboutAA {
+  margin: 0px auto;
+  padding: 20px;
+  font-size: 18px;
+  width: 100%;
+}
+
+.aboutA-item {
+  margin: 40px 0px;
+}
+
+.aboutTT {
+  font-size: 25px;
+  margin-bottom: 10px;
+}
+
+hr {
+  width: 15%;
+  margin-bottom: 10px;
+}
+
+/*B*/
+.aboutB {
+  max-width: 100%;
+  margin: 50px auto;
+  padding: 10px;
+}
+
+.aboutBB {
+  margin: 0px auto;
+  padding: 20px;
+  font-size: 18px;
+  width: 100%;
+}
+
+.aboutBB-item {
+  margin: 10px auto;
+  padding: 20px;
+  text-align: center;
+  color: #038686;
+}
+
+.aboutBB-text {
   font-size: 20px;
+  padding: 20px 0px 10px 0px;
+  width: 100%;
+}
+
+.aboutBB-img1 {
+  width: 130px;
+  height: 130px;
+  margin: 0px auto;
+  background-image: url("../assets/5.png");
+}
+
+.aboutBB-img2 {
+  width: 130px;
+  height: 130px;
+  margin: 0px auto;
+  background-image: url("../assets/6.png");
+}
+
+.aboutBB-img3 {
+  width: 130px;
+  height: 130px;
+  margin: 0px auto;
+  background-image: url("../assets/7.png");
+}
+
+
+
+/*響應式*/
+@media (min-width: 919px) {
+  .aboutA {
+    display: inline-flex;
+    justify-content: space-around;
+  }
+
+  .aboutBB {
+    display: inline-flex;
+    justify-content: space-around;
+  }
 }
 </style>
